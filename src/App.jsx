35a0947109b8f311df9c20c82ea4fb338803bf70
jmvaliente <valiente_jose2@hotmail.com>
2024-12-17 00:47:37 +0100

    feat: refactor App component to integrate Selector and Images components for breed selection and display

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,29 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Selector from './components/Selector'
+import Images from './components/Images'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [selectedBreed, setSelectedBreed] = useState('');
+  const [selectedSubBreed, setSelectedSubBreed] = useState('');
+
+  const handleBreedSelect = (breed) => {
+    setSelectedBreed(breed);
+    setSelectedSubBreed('');
+  };
+
+  const handleSubBreedSelect = (subBreed) => {
+    setSelectedSubBreed(subBreed);
+  };
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Selector handleBreedSelect={handleBreedSelect} selectedBreed={selectedBreed}  handleSubBreedSelect={handleSubBreedSelect} selectedSubBreed={selectedSubBreed}/>
       </div>
-      <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        {selectedBreed && <Images selectedBreed={selectedBreed} selectedSubBreed={selectedSubBreed}/>}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
